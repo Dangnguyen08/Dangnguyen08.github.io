ba2cb09d5628cdb35f737a5a8ad8a7e627f62b31
Dangnguyen08 <80185806+Dangnguyen08@users.noreply.github.com>
2021-07-29 15:32:27 +0700

    wi

---
+++ wi/Lesson 1/BTVN/bt1.js
@@ -0,0 +1,98 @@
+// Câu 1:
+let student = [
+    {
+        name: "NVA",
+        age: 16,
+        course: ["mindX 01", "mindX 02", "mindX 03"]
+    },
+    {
+        name: "NVB",
+        age: 18,
+        course: ["mindX 01", "mindX 02", "mindX 04"]
+    },
+    {
+        name: "NVC",
+        age: 17,
+        course: ["mindX 01", "mindX 03", "mindX 04"]
+    },
+    {
+        name: "NVD",
+        age: 15,
+        course: ["mindX 01", "mindX 02", "mindX 03"]
+    }
+]    
+
+
+// a)
+
+// let age = prompt(" Do tuoi can tim: ")
+// let age= 15
+// let result =[]
+// for (let index = 0; index < student.length; index++) {
+//     if(student[index].age == age){
+//         result.push(student[index].name)
+//     }
+// }
+// if(result.length > 0){
+//     result.forEach(item => {
+//         console.log(item)
+//     })
+// }else{
+//     console.log("Khong tim thay")
+// }
+// return
+
+// b)
+// let courseName = prompt(" Khoa hoc can tim? ")
+// let courseName = "mindX 01"
+// let result = []
+// for (let index = 0; index < student.length; index++) {
+//     if(student[index].course.includes(courseName)){
+//         result.push(student[index].name)
+//     }
+// }
+// if(result.length > 0){
+//     result.forEach(item => {
+//         console.log(item)
+//     })
+// }else{
+//     console.log("Chưa có lớp học này")
+// }
+
+
+
+// Câu 2:
+let arr1 = [0,2,4,6,8]
+let arr2 = [1,3,5,7]
+
+// a)
+// let result = []
+// arr1.forEach(num => {
+//     if(num>3){
+//         result.push(num)
+//     }
+// })
+// arr2.forEach(num2 => {
+//     if(num2>3){
+//         result.push(num2)
+//     }
+// })
+// console.log(result)
+
+// b)
+// let arr = []
+// arr1.forEach(num => {
+//     arr.push(num)
+// })
+// arr2.forEach(num2 => {
+//     if(num2==1){
+//         arr.push(2)
+//     }else{
+//         arr.push(num2)
+//     }
+    
+// })
+// console.log(arr)
+
+// c)
+// e chịu, e k biết làm :((
